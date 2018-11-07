a96eb33f3889787f2cdb20954b604d9cc82fe4a1
Drarerg <gerarddp86@gmail.com>
2018-11-07 23:30:04 +0100

    UPDATE 7-11 23:29

---
+++ PROTOTIP-1/Assets/AnimationTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+
+    Animator anim;
+
+    int one = Animator.StringToHash("jump");
+    //Make sure Trigger parameter in animator controller is wrote the same way that you string it//
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(one);
+        }
+    }
+}
---
+++ PROTOTIP-1/Assets/CartellTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartellTrigger : MonoBehaviour {
+    //private bool CartellTrigger = false;
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //CartellTrigger = true;
+        anim.SetTrigger("CartellTrigger");
+        Debug.Log("CartellTrigger");
+    }
+
+}
