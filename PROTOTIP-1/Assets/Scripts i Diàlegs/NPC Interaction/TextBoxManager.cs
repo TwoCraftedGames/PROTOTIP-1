97496ad367b4fa84072bbf60ec0ee0902e15a969
Drarerg <gerarddp86@gmail.com>
2018-11-13 22:24:51 +0100

    Update 13-11-2018 22:19
    (PROVES QUE HE ANAT FENT)
    "CANVI GRAN"
    - Hi ha una caixa de text per on (obivàment) hi pots insertar el text (duh) i s'avança amb enter, encara falta acabar de programar-ho de manera que el text sigui un "pop-up" quan interactuïs amb un NPC.
    
    "CANVIS MENORS"
    - Hi ha un nou "escenàri" (molt lleig tbh :help: no sé dibuixar el terra)
    - També hi ha un decorat que pretèn ser vertical (tot i que no ho aconsegueix gaire bé...).
    - Per últim hi ha 3 parets invisibles que funcionen com a "tope" del mapa.

---
+++ PROTOTIP-1/Assets/Scripts i Diàlegs/NPC Interaction/TextBoxManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextBoxManager: MonoBehaviour
+{
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public PlayerMovement1 player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement1>();
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+
+
+        }
+
+        if(endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+
+        }
+
+    }
+
+    private void Update()
+    {
+
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine += 1;
+
+        }
+
+        if(currentLine > endAtLine)
+        {
+            textBox.SetActive(false);
+        }
+    }
+
+}
